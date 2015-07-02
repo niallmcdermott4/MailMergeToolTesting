d46538d46c3e818abd393cef4a0dd302e57d6ce7
Andrew Pattison <andrew.pattison@brightfuture.co.uk>
2015-07-02 16:12:42 +0100

    Adde some more tests and making progress.

---
+++ UnitTests/RegularExpressionHelperTests.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTests
+{
+    using FluentAssertions;
+    using MailMergeTool;
+    using Xunit;
+
+    public class When_I_replace_pattern_in_string
+    {
+        [Fact]
+        public void It_should_replace_pattern_and_return_expected_value()
+        {
+            var helper = new RegularExpressionHelper();
+            var actual = helper.Replace("Hello <<Address>>", "World", "<<Address>>");
+
+            actual.Should().Be("Hello World");
+        }
+    }
+}
