55bfa69e257c7c876239cbe1f3f0f6c03dca5044
Andrew Pattison <andrew.pattison@brightfuture.co.uk>
2015-07-02 11:06:09 +0100

    Completed mail merge tool tests and made a start on document printer tests.

--- UnitTests/MailMergeTests.cs
+++ UnitTests/MailMergeTests.cs
@@ -1,7 +1,11 @@
 ﻿namespace UnitTests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
     using MailMergeTool;
     using NSubstitute;
+    using Ploeh.AutoFixture;
     using Properties;
     using Xunit;
 
@@ -10,21 +14,60 @@
         MailMerge tool;
         IDocumentPrinter printer;
         IPersonRepository repository;
+        Person[] persons;
+        List<Person> receivedPersons = new List<Person>(); 
+        List<Document> receivedDocuments = new List<Document>();
+        Document document;
 
         public When_merge_a_document()
         {
+            // Arrangement
+            persons = new Fixture().CreateMany<Person>(5).ToArray();
+
             repository = Substitute.For<IPersonRepository>();
+            repository.Persons().Returns(persons);
+
             printer = Substitute.For<IDocumentPrinter>();
+            printer.When(p => p.Print(Arg.Any<MergedDocument>())).Do(call =>
+            {
+                receivedPersons.Add(call.Arg<MergedDocument>().Person);
+                receivedDocuments.Add(call.Arg<MergedDocument>().Document);
+            });
+
             tool = new MailMerge(repository, printer);
 
-            Document document = new Document {TokenizedDocument = Resources.TestDocument};
+            document = new Document {TokenizedDocument = Resources.TestDocument};
+
+            // Act
             tool.MergeAndPrint(document);
         }
 
         [Fact]
         public void It_should_use_the_persons_in_the_repository()
         {
+            // Assert
             repository.Received().Persons();
+        }
+
+        [Fact]
+        public void It_should_invoke_Print_method_of_printer()
+        {
+            // Assert
+            printer.ReceivedWithAnyArgs(5).Print(null);
+        }
+
+        [Fact]
+        public void It_should_pass_person_to_printer()
+        {
+            // Assert
+            receivedPersons.ShouldBeEquivalentTo(persons);
+        }
+
+        [Fact]
+        public void It_should_pass_document_to_printer()
+        {
+            // Assert
+            receivedDocuments.Distinct().Single().ShouldBeEquivalentTo(document);
         }
     }
 }
